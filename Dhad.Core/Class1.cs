4bcaec2017e360d29df9a1ec68f28044e5d01079
Maamar Dliouah <maamardli@gmail.com>
2025-03-30 22:53:43 +0100

    Add project files.

---
+++ Dhad.Core/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Dhad.Core
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Dhad.IDE/Form1.cs
@@ -0,0 +1,10 @@
+namespace Dhad.IDE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
